d6b11b02d4b9922887646d04f816523f7d83a240
achalmoras <achalmoras@gmail.com>
2013-07-10 20:32:49 +0000

    Reading from index.html

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var string2read = fs.readFile(index.html);
+  var buf = new Buffer(string2read.length);
+  buf = string2read;
+  response.send('buf');
 });
 
 var port = process.env.PORT || 5000;
